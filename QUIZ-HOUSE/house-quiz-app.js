01d34e6f10e457066e602864229839cd028531e9
Paige Gorry <paigeegorry@gmail.com>
2018-11-12 11:06:03 -0800

    input fields set up for house quiz

--- QUIZ-HOUSE/house-quiz-app.js
+++ QUIZ-HOUSE/house-quiz-app.js
@@ -2,7 +2,28 @@ import html from '../html.js';
 
 function makeTemplate() {
     return html`
-    <h1>Hello World</h1>
+    <h2>What is your favorite color scheme?</h2>
+
+    <div>
+        <input type="radio" id="choice-a" name="house" value="a" checked>
+            <label for="choice-a">A. Scarlet and Gold</label>
+    </div>
+
+    <div>
+        <input type="radio" id="choice-b" name="house" value="b">
+            <label for="choice-b">B. Blue and Bronze</label>
+    </div>
+
+    <div>
+        <input type="radio" id="choice-c" name="house" value="c">
+            <label for="choice-c">C. Green and Silver</label>
+    </div>
+
+    <div>
+        <input type="radio" id="choice-d" name="house" value="d">
+            <label for="choice-d">D. Yellow and Black</label>
+    </div>
+
     `;
 }
 
