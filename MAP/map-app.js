938b75d6c78fe9eb9b36576e25e53a693b834308
Cheri A Portman <cheriportman@hotmail.com>
2018-11-12 14:29:43 -0800

    some css to organize things

--- MAP/map-app.js
+++ MAP/map-app.js
@@ -3,9 +3,8 @@ import html from '../html.js';
 function makeTemplate() {
     return html`
         <h1>Hello! Imma Map!</h1>
-        <img id="map-hat" src="../assets/sorting-hat.jpg" alt="A Sorting Hat">
         <a href="../QUIZ-HOUSE/house-quiz.html"><img id="map-house" src="../assets/houses.jpg" alt="House Medallions"></a>
-        <a href="../END-STORY/end-story.html"><img id="map-door" src="../assets/door.jpg" alt="A Secret Door - Your Future?"></a>
+        <a href="../END-STORY/end-story.html"><img id="map-door" src="../assets/door.jpg" alt="A Secret Door - Your Future?" width="400" height="280"></a>
         `;
 }
 
