a9569376a29879c4983acd6bde0940ae0821a24f
Cheri A Portman <cheriportman@hotmail.com>
2018-11-12 16:09:51 -0800

    end story display for house is wired up

--- END-STORY/end-story-display.js
+++ END-STORY/end-story-display.js
@@ -8,20 +8,25 @@ console.log('dataAPI', userObj);
 
 function makeTemplate() {
     return html`
-    <div id="">Test!</div>
+    <div id="house">Test!</div>
     `;
 }
 
-if(userObj.house === 'a'){
-    
-} else if(userObj.house === 'b') {
-    
-}
 
 export default class DisplayText{
-
+    
     render() {
         const dom = makeTemplate();
+        const displayContainer = dom.querySelector('#house');
+        if(userObj.house === 'a'){
+            displayContainer.textContent = 'your house choice a story here';
+        } else if(userObj.house === 'b') {
+            displayContainer.textContent = 'your house choice b story here!';
+        } else if(userObj.house === 'c') {
+            displayContainer.textContent = 'your house choice c story here';
+        } else {
+            displayContainer.textContent = 'your house choice d story here.';
+        }
         return dom;
     }
 }
