456010c7b891e684938188492e00d446dfa098a0
Cheri A Portman <cheriportman@hotmail.com>
2018-11-13 16:48:00 -0800

    map is gridded up and images shake on hover

--- MAP/map-app.js
+++ MAP/map-app.js
@@ -6,16 +6,16 @@ let user = resultsApi.getAll();
 function makeTemplate() {
     return html`
         <div id="quiz-icon">
-            <a href="../HOME/home.html">Home</a>
-            <a href="../QUIZ-HOUSE/house-quiz.html"><img id="map-house" src="../assets/Hogwarts-Logo-PNG-Transparent.png" alt="Hogwarts Crest"></a>
-            <a href="../QUIZ-PATRONUS/patronus-quiz.html"><img id="map-patronus" src="../assets/group-patronus.jpg" alt="Group of Patronus"></a>
-            <a href="../QUIZ-WAND/wand-quiz.html"><img id="map-wand" src="../assets/Three-wands.png" alt="a trio of wands"></a>
-            <a href="../QUIZ-CAREER/career-quiz.html"><img id="map-career" src="../assets/ministry-magic.png" alt="Ministry of Magic Seal"></a>
-            <a href="../END-STORY/end-story.html"><img id="map-door" src="../assets/fancier-door.png" alt="A Secret Door - Your Future?"></a>
+            <a href="../QUIZ-HOUSE/house-quiz.html" id="map-house"><img src="../assets/Hogwarts-Logo-PNG-Transparent.png" alt="Hogwarts Crest"></a>
+            <a href="../QUIZ-PATRONUS/patronus-quiz.html" id="map-patronus" ><img src="../assets/group-patronus.jpg" alt="Group of Patronus"></a>
+            <a href="../QUIZ-WAND/wand-quiz.html" id="map-wand"><img  src="../assets/Three-wands.png" alt="a trio of wands"></a>
+            <a href="../QUIZ-CAREER/career-quiz.html" id="map-career"><img  src="../assets/ministry-magic.png" alt="Ministry of Magic Seal"></a>
+            <a href="../END-STORY/end-story.html" id="map-door" ><img src="../assets/fancier-door.png" alt="A Secret Door - Your Future?"></a>
         </div>
         `;
-}
-
+    }
+    
+    // <a href="../HOME/home.html">Home</a>
 
 
 export default class App {
